67f35ce6870676c7253d6f32e2b351c65d502fb2
Mark Cagas <90206115+nic0-dev@users.noreply.github.com>
2024-03-01 07:46:47 +0000

    Deployed Smart Contract on Arbiscan Sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+import "dotenv/config";
+
+const ARBITRUM_SEPOLIA_URL: string = process.env.ARBITRUM_SEPOLIA_URL ?? "";
+const PRIVATE_KEY: string = process.env.PRIVATE_KEY ?? "";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    arbitrumSepolia: {
+      url: ARBITRUM_SEPOLIA_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 421614,
+    }
+  }
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,21 +1,10 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = ethers.parseEther("0.001");
-
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
+  const lock = await ethers.deployContract("Token");
   await lock.waitForDeployment();
-
   console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+    ` deployed to ${lock.target}`
   );
 }
 
